b981aec60b608ccb13e4d38731da7ad1afc6d115
jack <pwcasdf@gmail.com>
2017-04-11 19:37:26 +0900

    done!!

--- IntelligentKiosk/MainPage.xaml.cs
+++ IntelligentKiosk/MainPage.xaml.cs
@@ -21,13 +21,14 @@ using Windows.Media.Core;
 
 using Windows.Media.Capture;
 using Windows.ApplicationModel;
-using System.Threading.Tasks;
 using Windows.System.Display;
 using Windows.Graphics.Display;
 using System.Diagnostics;
 using Windows.Devices.Enumeration;
 using Windows.Foundation.Metadata;
 using Windows.UI.ViewManagement;
+using Windows.Media;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -39,11 +40,14 @@ namespace IntelligentKiosk
     public sealed partial class MainPage : Page
     {
         private MediaCapture _mediaCapture;
-        private DisplayRequest _displayRequest;
-        private bool _isPreviewing;
-        private bool _isInitialized;
-        private bool _mirroringPreview;
-        private bool _externalCamera;
+        private DisplayRequest _displayRequest=new DisplayRequest();
+        private readonly DisplayInformation _displayInformation = DisplayInformation.GetForCurrentView();
+        private readonly SystemMediaTransportControls _systemMediaControls = SystemMediaTransportControls.GetForCurrentView();
+
+        private bool _isPreviewing=false;
+        private bool _isInitialized=false;
+        private bool _mirroringPreview=false;
+        private bool _externalCamera=false;
 
 
         public MainPage()
@@ -54,53 +58,74 @@ namespace IntelligentKiosk
             _mediaPlayerElement.Source = MediaSource.CreateFromUri(manifestUri);
             _mediaPlayerElement.MediaPlayer.Play();
 
+            NavigationCacheMode = NavigationCacheMode.Required;
+
+
             Application.Current.Suspending += Application_Suspending;
             Application.Current.Resuming += Application_Resuming;
         }
 
 
-        // start camera view  @jack
+        // start preview async  @jack
         private async Task StartPreviewAsync()
         {
-            try
-            {
-                _mediaCapture = new MediaCapture();
-                await _mediaCapture.InitializeAsync();
+            Debug.WriteLine("StartPreviewAsync");
 
-                PreviewControl.Source = _mediaCapture;
-                await _mediaCapture.StartPreviewAsync();
-                _isPreviewing = true;
+            // prevent sleep mode  @jack
+            _displayRequest.RequestActive();
 
-                // prevent sleep mode  @jack
-                _displayRequest.RequestActive();
-                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
-            }
-            catch (UnauthorizedAccessException)
+            _systemMediaControls.PropertyChanged += SystemMediaControls_PropertyChanged;
+
+            PreviewControl.Source = _mediaCapture;
+            PreviewControl.FlowDirection = _mirroringPreview ? FlowDirection.RightToLeft : FlowDirection.LeftToRight;
+
+            // start the preview  @jack
+            await _mediaCapture.StartPreviewAsync();
+            _isPreviewing = true;
+        }
+
+        // stop preview async  @jack
+        private async Task StopPreviewAsync()
+        {
+            _isPreviewing = false;
+            await _mediaCapture.StopPreviewAsync();
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                // when the camera is in privacy settings  @jack
-                System.Diagnostics.Debug.Write("The app was denied access to the camera");
-            }
+                PreviewControl.Source = null;
+
+                _displayRequest.RequestRelease();
+            });
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await InitializeCameraAsync();
         }
 
         private async Task InitializeCameraAsync()
         {
             Debug.WriteLine("InitializeCameraAync");
 
-            if(_mediaCapture==null)
+            if (_mediaCapture == null)
             {
+                // attempt to get the back camera if one is availale, but use any camera device if not  @jack
                 var cameraDevice = await FindCameraDeviceByPanelAync(Windows.Devices.Enumeration.Panel.Back);
 
-                if(cameraDevice==null)
+                if (cameraDevice == null)
                 {
                     Debug.WriteLine("No camera device found!");
                     return;
                 }
 
                 _mediaCapture = new MediaCapture();
+
+                // notification when something goes wrong  @jack
                 _mediaCapture.Failed += MediaCapture_Failed;
 
                 var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
 
+                // initialize mediacapture  @jack
                 try
                 {
                     await _mediaCapture.InitializeAsync(settings);
@@ -111,6 +136,7 @@ namespace IntelligentKiosk
                     Debug.WriteLine("The app was denied access to the camera");
                 }
 
+                // if initialization succeeded  @jack
                 if (_isInitialized)
                 {
                     if (cameraDevice.EnclosureLocation == null || cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Unknown)
@@ -122,41 +148,32 @@ namespace IntelligentKiosk
                         _externalCamera = false;
 
                         _mirroringPreview = (cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front);
-
-                        await StartPreviewAsync();
                     }
+
+                    await StartPreviewAsync();
                 }
             }
         }
 
+        // free the camera resource  @jack
         private async Task CleanupCameraAsync()
         {
-            if (_mediaCapture != null)
+            if (_isInitialized)
             {
-                await _mediaCapture.StopPreviewAsync();
-            }
-
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                PreviewControl.Source = null;
-                if (_displayRequest != null)
+                if (_isPreviewing)
                 {
-                    _displayRequest.RequestRelease();
+                    await StopPreviewAsync();
                 }
 
+                _isInitialized = false;
+            }
+
+            if (_mediaCapture != null)
+            {
+                _mediaCapture.Failed -= MediaCapture_Failed;
                 _mediaCapture.Dispose();
                 _mediaCapture = null;
-            });
-        }
-
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
-        {
-            await InitializeCameraAsync();
-        }
-
-        protected async override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            await CleanupCameraAsync();
+            }
         }
 
         private async void Application_Resuming(object sender, object o)
@@ -190,7 +207,27 @@ namespace IntelligentKiosk
             Debug.WriteLine("MediaCapture_Failed: (0x{0:X}) {1}", errorEventArgs.Code, errorEventArgs.Message);
 
             await CleanupCameraAsync();
-        }
+        }  
 
+        private async void SystemMediaControls_PropertyChanged(SystemMediaTransportControls sender, SystemMediaTransportControlsPropertyChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                // Only handle this event if this page is currently being displayed
+                if (args.Property == SystemMediaTransportControlsProperty.SoundLevel && Frame.CurrentSourcePageType == typeof(MainPage))
+                {
+                    // Check to see if the app is being muted. If so, it is being minimized.
+                    // Otherwise if it is not initialized, it is being brought into focus.
+                    if (sender.SoundLevel == SoundLevel.Muted)
+                    {
+                        await CleanupCameraAsync();
+                    }
+                    else if (!_isInitialized)
+                    {
+                        await InitializeCameraAsync();
+                    }
+                }
+            });
+        }
     }
 }

2be424156a5518ea54106f4ff5f2e415e4591644
jack <pwcasdf@gmail.com>
2017-04-11 17:37:45 +0900

    streaming vedio done

--- IntelligentKiosk/MainPage.xaml.cs
+++ IntelligentKiosk/MainPage.xaml.cs
@@ -13,6 +13,22 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Windows.Media.Streaming.Adaptive;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.Media.Playback;
+using Windows.Media.Core;
+
+using Windows.Media.Capture;
+using Windows.ApplicationModel;
+using System.Threading.Tasks;
+using Windows.System.Display;
+using Windows.Graphics.Display;
+using System.Diagnostics;
+using Windows.Devices.Enumeration;
+using Windows.Foundation.Metadata;
+using Windows.UI.ViewManagement;
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace IntelligentKiosk
@@ -22,9 +38,159 @@ namespace IntelligentKiosk
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MediaCapture _mediaCapture;
+        private DisplayRequest _displayRequest;
+        private bool _isPreviewing;
+        private bool _isInitialized;
+        private bool _mirroringPreview;
+        private bool _externalCamera;
+
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Uri manifestUri = new Uri("");
+            _mediaPlayerElement.Source = MediaSource.CreateFromUri(manifestUri);
+            _mediaPlayerElement.MediaPlayer.Play();
+
+            Application.Current.Suspending += Application_Suspending;
+            Application.Current.Resuming += Application_Resuming;
         }
+
+
+        // start camera view  @jack
+        private async Task StartPreviewAsync()
+        {
+            try
+            {
+                _mediaCapture = new MediaCapture();
+                await _mediaCapture.InitializeAsync();
+
+                PreviewControl.Source = _mediaCapture;
+                await _mediaCapture.StartPreviewAsync();
+                _isPreviewing = true;
+
+                // prevent sleep mode  @jack
+                _displayRequest.RequestActive();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // when the camera is in privacy settings  @jack
+                System.Diagnostics.Debug.Write("The app was denied access to the camera");
+            }
+        }
+
+        private async Task InitializeCameraAsync()
+        {
+            Debug.WriteLine("InitializeCameraAync");
+
+            if(_mediaCapture==null)
+            {
+                var cameraDevice = await FindCameraDeviceByPanelAync(Windows.Devices.Enumeration.Panel.Back);
+
+                if(cameraDevice==null)
+                {
+                    Debug.WriteLine("No camera device found!");
+                    return;
+                }
+
+                _mediaCapture = new MediaCapture();
+                _mediaCapture.Failed += MediaCapture_Failed;
+
+                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                try
+                {
+                    await _mediaCapture.InitializeAsync(settings);
+                    _isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Debug.WriteLine("The app was denied access to the camera");
+                }
+
+                if (_isInitialized)
+                {
+                    if (cameraDevice.EnclosureLocation == null || cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Unknown)
+                    {
+                        _externalCamera = true;
+                    }
+                    else
+                    {
+                        _externalCamera = false;
+
+                        _mirroringPreview = (cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front);
+
+                        await StartPreviewAsync();
+                    }
+                }
+            }
+        }
+
+        private async Task CleanupCameraAsync()
+        {
+            if (_mediaCapture != null)
+            {
+                await _mediaCapture.StopPreviewAsync();
+            }
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                PreviewControl.Source = null;
+                if (_displayRequest != null)
+                {
+                    _displayRequest.RequestRelease();
+                }
+
+                _mediaCapture.Dispose();
+                _mediaCapture = null;
+            });
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await InitializeCameraAsync();
+        }
+
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            await CleanupCameraAsync();
+        }
+
+        private async void Application_Resuming(object sender, object o)
+        {
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                await InitializeCameraAsync();
+            }
+        }
+
+        private async void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+                await CleanupCameraAsync();
+                deferral.Complete();
+            }
+        }
+
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAync(Windows.Devices.Enumeration.Panel desiredPanel)
+        {
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
+            return desiredDevice ?? allVideoDevices.FirstOrDefault();
+        }
+
+        private async void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            Debug.WriteLine("MediaCapture_Failed: (0x{0:X}) {1}", errorEventArgs.Code, errorEventArgs.Message);
+
+            await CleanupCameraAsync();
+        }
+
     }
 }

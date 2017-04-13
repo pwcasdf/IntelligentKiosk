8b71dfa00dc271690e72c78b15ad4609ce79e23c
pwcas <pwcas@DESKTOP-NHJFAKQ>
2017-04-13 18:54:38 +0900

    4/13 camera_rework on-going 1

---
+++ IntelligentKiosk/Controls/EmotionEmojiControl.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.ProjectOxford.Common.Contract;
+using ServiceHelpers;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace IntelligentKiosk.Controls
+{
+    public partial class EmotionEmojiControl : UserControl
+    {
+        public EmotionEmojiControl()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateEmotion(EmotionScores scores)
+        {
+            EmotionData topEmotion = EmotionServiceHelper.ScoresToEmotionData(scores).OrderByDescending(d => d.EmotionScore).First();
+            string label = "", emoji = "";
+
+            switch (topEmotion.EmotionName)
+            {
+                case "Anger":
+                    label = "Angry";
+                    emoji = "\U0001f620";
+                    break;
+                case "Contempt":
+                    label = "Contemptuous";
+                    emoji = "\U0001f612";
+                    break;
+                case "Disgust":
+                    label = "Disgusted";
+                    emoji = "\U0001f627";
+                    break;
+                case "Fear":
+                    label = "Afraid";
+                    emoji = "\U0001f628";
+                    break;
+                case "Happiness":
+                    label = "Happy";
+                    emoji = "\U0001f60a";
+                    break;
+                case "Neutral":
+                    label = "Neutral";
+                    emoji = "\U0001f614";
+                    break;
+                case "Sadness":
+                    label = "Sad";
+                    emoji = "\U0001f622";
+                    break;
+                case "Surprise":
+                    label = "Surprised";
+                    emoji = "\U0001f632";
+                    break;
+                default:
+                    break;
+            }
+
+            this.emotionEmoji.Text = emoji;
+            this.emotionText.Text = label;
+        }
+    }
+}
---
+++ IntelligentKiosk/KioskExperiences.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace IntelligentKiosk
+{
+    public static class KioskExperiences
+    {
+        private static readonly Type[] expTypes;
+
+        static KioskExperiences()
+        {
+            expTypes = typeof(KioskExperiences).GetTypeInfo().Assembly.GetTypes().Where(t =>
+                t.Namespace == "IntelligentKioskSample.Views"
+                && t.GetTypeInfo().GetCustomAttribute<KioskExperienceAttribute>() != null)
+                .ToArray();
+        }
+
+        public static IEnumerable<KioskExperience> Experiences
+        {
+            get
+            {
+                return expTypes.Select(t => new KioskExperience()
+                {
+                    PageType = t,
+                    Attributes = t.GetTypeInfo().GetCustomAttribute<KioskExperienceAttribute>()
+                }).OrderBy(e => e.Attributes.Title);
+            }
+        }
+    }
+
+    public class KioskExperience
+    {
+        public Type PageType { get; set; }
+        public KioskExperienceAttribute Attributes { get; set; }
+    }
+
+    public enum ExperienceType
+    {
+        Kiosk,
+        Other
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class KioskExperienceAttribute : Attribute
+    {
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public ExperienceType ExperienceType { get; set; }
+    }
+}
